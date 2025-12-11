9ee21d0391228ebeadd9a4fe223a6da932ad9604
ryancastromiranda17-a11y <ryancastromiranda17@gmail.com>
2025-12-11 02:25:21 -0300

    versão 2
    frontend da pagina principal

---
+++ Financia/static/js/script.js
@@ -0,0 +1,81 @@
+console.log("STATIC OK");
+
+
+const ctx = document.getElementById('evolucaoMensal');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [
+            {
+                label: 'Receitas',
+                data: [5800, 6000, 5000, 7500, 6400, 6500],
+                backgroundColor: '#0FAF68',
+                borderRadius: 8,
+                barPercentage: 0.5
+            },
+            {
+                label: 'Despesas',
+                data: [4000, 4800, 3700, 5100, 4300, 4200],
+                backgroundColor: '#E63946',
+                borderRadius: 8,
+                barPercentage: 0.5
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'bottom'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    callback: (value) => value / 1000 + 'k'
+                },
+                grid: { color: '#dcdcdc' }
+            },
+            x: {
+                grid: { display: false }
+            }
+        }
+    }
+});
+
+
+// =======================
+// GRÁFICO - DESPESAS POR CATEGORIA
+// =======================
+
+const ctx2 = document.getElementById("graficoCategorias");
+
+new Chart(ctx2, {
+    type: "pie",
+    data: {
+        labels: ["Moradia", "Alimentação", "Compras", "Educação", "Contas"],
+        datasets: [{
+            data: [1800, 607.55, 320, 297, 180.45],
+            backgroundColor: [
+                "#0C1E66",  // Moradia
+                "#FF4B3E",  // Alimentação
+                "#DA13B8",  // Compras
+                "#1BA7F0",  // Educação
+                "#FFDD33"   // Contas (amarelo igual a imagem)
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false // tira legenda do Chart.js (pois você já tem lista do lado)
+            }
+        }
+    }
+});
+
